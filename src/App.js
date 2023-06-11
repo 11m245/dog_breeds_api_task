efa5796b24b179ff174baf9ffef13570bbbc7fc5
11m245 <sivaraj2siva@gmail.com>
2023-06-11 19:26:15 +0530

    search dog breeds task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Search } from "./Search";
+import { Table } from "./Table";
+import { toast } from "react-toastify";
 
 function App() {
+  const [data, setData] = useState({});
+  const [searchKeyWord, setSearchKeyWord] = useState("");
+  const [filteredKeys, setFilteredKeys] = useState([]);
+
+  async function getBreedsData() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    if (response.status === 200) {
+      const fetchedData = await response.json();
+      setData(fetchedData.message);
+      console.log("fet", fetchedData.message);
+      setFilteredKeys(Object.keys(fetchedData.message));
+      toast.success("fetched");
+    } else {
+      toast.error(response.statusText);
+    }
+  }
+  useEffect(() => {
+    getBreedsData();
+  }, []);
+
+  useEffect(() => {
+    if (searchKeyWord) {
+      const filteredArray = Object.keys(data).filter((key) =>
+        key.toLowerCase().includes(searchKeyWord.toLowerCase())
+      );
+      // console.log("filtered data", filteredArray);
+      setFilteredKeys(filteredArray);
+    } else {
+      setFilteredKeys(Object.keys(data));
+    }
+  }, [searchKeyWord]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header section">
+        <h1 className="page-title">Search Dog Breeds API Task </h1>
+        <Search
+          searchKeyWord={searchKeyWord}
+          setSearchKeyWord={setSearchKeyWord}
+        />
       </header>
+      <main className="section">
+        <Table filteredKeys={filteredKeys} data={data} />
+      </main>
     </div>
   );
 }
